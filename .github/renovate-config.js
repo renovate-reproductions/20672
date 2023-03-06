bf4aa7bd0ec46318647e88bfcebe0bc8e8a642ef
grillmeistertodafleisch <2silly4u@web.de>
2023-03-06 16:06:31 +0100

    fix: renovate config rework

---
+++ .github/renovate-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    allowPostUpgradeCommandTemplating: "true",
+    allowedPostUpgradeCommands:["^echo .*"],
+    branchPrefix: 'renovate/',
+    username: 'renovate-repro-20672[bot]',
+    gitAuthor: 'Self-hosted Renovate Bot <79356887+renovate-repro-20672[bot]@users.noreply.github.enterprise.com>',
+    onboarding: false,
+    platform: 'github',
+    includeForks: true,
+    dryRun: 'full',
+    repositories: [
+      'MDietz135/renovate-repro-20672'
+    ],
+  };
