c1bbd6f5dade8cc55f446d3a2512f2691300eeee
grillmeistertodafleisch <2silly4u@web.de>
2023-03-06 16:57:02 +0100

    chore: enable onboarding

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,25 +1,11 @@
 module.exports = {
   allowPostUpgradeCommandTemplating: "true",
   allowedPostUpgradeCommands:["^echo .*"],
-  branchPrefix: 'renovate/',
-  username: 'renovate-repro-20672[bot]',
-  gitAuthor: 'Self-hosted Renovate Bot <79356887+renovate-repro-20672[bot]@users.noreply.github.enterprise.com>',
-  onboarding: false,
-  platform: 'github',
-  repositories: [
-    'MDietz135/renovate-repro-20672'
-  ],
+  branchPrefix: "renovate/",
+  username: "renovate-repro-20672[bot]",
+  gitAuthor: "Self-hosted Renovate Bot <79356887+renovate-repro-20672[bot]@users.noreply.github.enterprise.com>",
+  onboarding: true,
+  platform: "github",
+  repositories: ["MDietz135/renovate-repro-20672"],
   skipInstalls: false,
-  baseBranches: ["main"],
-  packageRules: [
-    {
-      updateTypes: ["minor", "patch", "pin", "digest"],
-      automerge: false,
-    }
-  ],
-  postUpgradeTasks: {
-    commands: ["echo '{{ upgrades }}'", "echo '{{ upgrades }}' > upgrades.json"],
-    fileFilters: ["upgrades.json"],
-    executionMode: "update"
-  }
 };
