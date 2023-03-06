5e3aef0e2f56ae6af67a8da7b771b50c273d169b
grillmeistertodafleisch <2silly4u@web.de>
2023-03-06 16:22:46 +0100

    chore: refactor renovate stuff

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -6,9 +6,8 @@ module.exports = {
     gitAuthor: 'Self-hosted Renovate Bot <79356887+renovate-repro-20672[bot]@users.noreply.github.enterprise.com>',
     onboarding: false,
     platform: 'github',
-    includeForks: true,
-    dryRun: 'full',
     repositories: [
       'MDietz135/renovate-repro-20672'
     ],
+    skipInstalls: false,
   };
